98298d4e581835710b9270905ae4c46f37b58592
weldsh2535 <74667144+weldsh2535@users.noreply.github.com>
2023-01-23 16:34:53 +0300

    first commit

--- app/src/main/java/com/example/sampleapps/MainActivity.kt
+++ app/src/main/java/com/example/sampleapps/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.sampleapps
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.example.sampleapps.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.submit.setOnClickListener {
+            Toast.makeText(applicationContext,"FullName is ${binding.firstname.text.toString()} and ${binding.lastName.text.toString()}",Toast.LENGTH_SHORT).show()
+          }
+
+
     }
 }
